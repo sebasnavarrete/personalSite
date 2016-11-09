91fb768dfb56855a33d3a4a89066c0f186906769
Sebastián Navarrete <Sebastin Navarrete>
2016-11-09 15:59:09 -0500

    Agregado el analytics de piwik

--- resources/views/base/layout.blade.php
+++ resources/views/base/layout.blade.php
@@ -46,7 +46,31 @@
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.sebastian-navarrete.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//analytics.sebastian-navarrete.com/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-72659243-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+<!-- End Piwik Code -->
 
 </head>
 
@@ -86,15 +110,5 @@
 <script src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
 {!! HTML::script('public/assets/js/jquery.fitvids.js') !!}
 {!! HTML::script('public/assets/js/scripts.js') !!}
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-72659243-1', 'auto');
-    ga('send', 'pageview');
-
-</script>
 </body>
 </html>
