1a4349c74b68496d939d646cfee9779f64d58953
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2017-08-03 09:34:42 -0500

    enlaces fix

--- resources/views/base/layout.blade.php
+++ resources/views/base/layout.blade.php
@@ -50,8 +50,8 @@
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r;
             i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
             a = s.createElement(o),
                 m = s.getElementsByTagName(o)[0];
             a.async = 1;
@@ -101,7 +101,7 @@
 {!! HTML::script('assets/js/jquery.magnific-popup.min.js') !!}
 {!! HTML::script('assets/js/jquery.fitvids.js') !!}
 {!! HTML::script('assets/js/scripts.js') !!}
-<script src="https://a.vimeocdn.com/js/froogaloop2.min.js"></script>
+{!! HTML::script('assets/js/froogaloop2.min.js') !!}
 <script type="application/javascript">
     function callbackCaptcha() {
         $('#formContactSubmit').removeAttr('disabled');
@@ -109,14 +109,14 @@
 </script>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/5934c1ba4374a471e7c51637/default';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5934c1ba4374a471e7c51637/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
     })();
 </script>
 <!--End of Tawk.to Script-->
