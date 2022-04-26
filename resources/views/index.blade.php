3a20d0c0ed38ab2efbf04dd683992cddbc0cccc8
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2022-04-26 09:53:52 -0500

    server change, image update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,6 @@
             <div class="preload-placeholder"></div>
         </div>
     </div>
-
-
     <!-- Home Section -->
     <section id="home" class="tt-fullHeight" data-stellar-vertical-offset="50" data-stellar-background-ratio="0.2">
         <div class="intro">
@@ -20,7 +18,7 @@
                 </div>
             @endif
             <h1>SEBASTIAN NAVARRETE</h1>
-            <h1><span>SOFTWARE DEVELOPER</span></h1>
+            <h1><span>Team leader and senior software engineer</span></h1>
             <p>I consider myself a proactive and resilient person, who never stops learning.
                 <br> I am always looking to provide solutions and generating value through technology.</p>
             <div class="social-icons">
@@ -37,8 +35,6 @@
             <div class="wheel"></div>
         </div>
     </section><!-- End Home Section -->
-
-
     <!-- Navigation -->
     <header class="header">
         <nav class="navbar navbar-custom" role="navigation">
@@ -59,7 +55,7 @@
                         <li><a href="#about">Profile</a></li>
                         <li><a href="#skills">Skills</a></li>
                         <li><a href="#works">Projects</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="https://wa.me/573134737213" target="_blank">Contact</a></li>
                     </ul>
                 </div>
             </div><!-- .container -->
@@ -71,9 +67,7 @@
     <section id="about" class="about-section section-padding">
         <div class="container">
             <h2 class="section-title wow fadeInUp">PROFESSIONAL PROFILE</h2>
-
             <div class="row">
-
                 <div class="col-md-4 col-md-push-8">
                     <div class="biography">
                         <ul>
@@ -93,41 +87,47 @@
                         </ul>
                     </div>
                 </div> <!-- col-md-4 -->
-
                 <div class="col-md-8 col-md-pull-4">
-
                     <div class="short-info wow fadeInUp">
                         <h3>Profile</h3>
                         <p>
-                            I'm Systems Engineer and Project Management specialist.
-                            Fullstack developer seeking to apply good development practices, readable and clean code.
-                            Always looking for optimal and innovative solutions and ready to learn new technologies.
-                            My skills are:
+                            I have loved programming since I did it for the first time. Ever since I was a kid, 
+                            and started doing animations and websites, technology and innovation has been my passion. 
+                            In the beginning, this coding activity was just for fun, but when I started my University degree, 
+                            I realized that I could build amazing things using a computer and I totally loved it. Of course, 
+                            I've been obsessed with the idea of solving problems using software.
+                        </p>
+                        <p>
+                            Before getting my University degree, two of my friends and I had our first professional 
+                            contract with a Startup, we were given the opportunity to build a software for creating 
+                            electrical infrastructure maps using PHP, Javascript (Jquery) and my SQL. My second contract 
+                            involved the use of AngularJS to build web applications. This experience allowed me to access 
+                            an internship with IBM to develop using .NET and achieve my graduation.
+                        </p>
+                        <p>
+                            Since I graduated, I have been working as a freelance for multiple companies, UN agencies, 
+                            NGOs and startups developing software (web and mobile -hybrid-) and leading development teams. 
+                            Some of my skills are:
                         </p>
 
                         <ul class="list-check">
-                            <li>Symfony - Laravel - Codeigniter - ExpressJs</li>
-                            <li>HTML5 - CSS3 - Bootstrap - Material Design</li>
+                            <li>ExpressJs - Laravel - Codeigniter</li>
                             <li>PHP - NodeJS - MySQL - PostgreSQL</li>
-                            <li>Javascript - Jquery - Angular - React</li>
+                            <li>HTML5 - CSS3 - Bootstrap - Material Design</li>
+                            <li>Javascript - Angular - React - Jquery</li>
                             <li>Linux - AWS - Google Cloud</li>
                             <li>Ionic - PowerBI - Google Data Studio</li>
                             <li>SCRUM</li>
                             <li>GIT</li>
                         </ul>
                     </div>
-
                     <div class="download-button">
-                        <a class="btn btn-info btn-lg" href="#contact"><i class="fa fa-paper-plane"></i>Contact me</a>
+                        <a class="btn btn-info btn-lg" target="_blank" href="https://wa.me/573134737213"><i class="fa fa-paper-plane"></i>Contact me</a>
                     </div>
                 </div>
-
-
             </div> <!-- /.row -->
         </div>
     </section><!-- End About Section -->
-
-
     <!-- Hire Section -->
     <section class="hire-section text-center" data-stellar-vertical-offset="50" data-stellar-background-ratio="0.2">
         <div class="hire-section-bg">
@@ -137,13 +137,12 @@
                         <h2 style="line-height: 40px;">
                             Do you have an idea or project? I can support you.
                         </h2>
-                        <a href="#contact" class="btn btn-default">Let's talk!</a>
+                        <a href="https://wa.me/573134737213" target="_blank" class="btn btn-default">Let's talk!</a>
                     </div><!-- /.col-md-12 -->
                 </div><!-- /.row -->
             </div>
         </div><!-- /.hire-section-bg -->
     </section><!-- End Hire Section -->
-
     <!-- Skills Section -->
     <section id="skills" class="skills-section section-padding">
         <div class="container">
@@ -184,7 +183,6 @@
                         </div><!-- /.progress -->
                     </div><!-- /.skill-progress -->
                 </div><!-- /.col-md-6 -->
-
                 <div class="col-md-6">
                     <div class="skill-progress">
                         <div class="skill-title"><h6>C# - .NET - SQL Server</h6></div>
@@ -220,11 +218,9 @@
                     </div><!-- /.skill-progress -->
                 </div><!-- /.col-md-6 -->
             </div><!-- /.row -->
-
             <div class="skill-chart text-center">
                 <h3>Other Skills</h3>
             </div>
-
             <div class="row more-skill text-center">
                 <div class="col-xs-12 col-sm-4 col-md-4">
                     <div class="chart" data-percent="80" data-color="e74c3c">
@@ -251,21 +247,17 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </section><!-- End Skills Section -->
-
     <!-- Works Section -->
     <section id="works" class="works-section section-padding">
         <div class="container">
             <h2 class="section-title wow fadeInUp">Some of my projects / Clients</h2>
-
             <ul class="list-inline" id="filter">
                 <li><a class="active" data-group="all">All</a></li>
                 <li><a class="active" data-group="web">Web</a></li>
                 <li><a class="active" data-group="mobile">Mobile</a></li>
             </ul>
-
             <div class="row">
                 <div id="grid">
                     <div class="portfolio-item col-xs-12 col-sm-4 col-md-3"
@@ -385,51 +377,4 @@
             </div><!-- /.row -->
         </div>
     </section><!-- End Works Section -->
-
-
-    <!-- Contact Section -->
-    <section id="contact" class="contact-section section-padding">
-        <div class="container">
-            <h2 class="section-title wow fadeInUp">Contact</h2>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="contact-form">
-                        <strong>Send Message</strong>
-                        {!! FORM::open(['route' => 'send', 'method' => 'post']) !!}
-                        <div class="form-group">
-                            <label for="InputName1">Name</label>
-                            <input type="text" name="name" class="form-control" id="InputName1" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="InputEmail1">Email</label>
-                            <input type="email" name="email" class="form-control" id="InputEmail1" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="InputSubject">Subject</label>
-                            <input type="text" name="subject" class="form-control" id="InputSubject" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="InputTextarea">Message</label>
-                            <textarea name="body" class="form-control" id="InputTextarea" rows="5" required></textarea>
-                        </div>
-                        <div data-callback="callbackCaptcha" class="g-recaptcha"
-                             data-sitekey="6LczJg8UAAAAACYkGYO1YsHNYmgzDNydAFidMAnF"></div>
-                        <button disabled id="formContactSubmit" type="submit" name="submit" class="btn btn-primary">
-                            Send
-                        </button>
-                        </form>
-                    </div>
-                </div><!-- /.col-md-6 -->
-
-                <div class="col-sm-6">
-                    <i class="fa fa-mobile"></i>
-                    <div class="contact-number">
-                        <strong>Phone</strong>
-                        <a style="color: #000;" href="tel:+573134737213">(+57) 313 4737213</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section><!-- End Contact Section -->
 @endsection
